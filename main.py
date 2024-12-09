f7f522fc5ff43de48fcca1e1d0a4fec49cc17824
miniMagic-beep <157633145+miniMagic-beep@users.noreply.github.com>
2024-12-09 17:48:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -194,13 +194,68 @@ class AlexNet(nn.Module):
             out = self.layer4(out)
             out = self.layer5(out)
             #print("After Conv shape:", out.shape)
-            out = out.reshape(out.size(0), -1)
+            #out = out.reshape(out.size(0), -1)
+            out = torch.flatten(out, start_dim=1)
             #print("After layer2 shape:", out.shape)
             out = self.fc(out)
             out = self.fc1(out)
             out = self.fc2(out)
             return out
+
+# class AlexNet(nn.Module):
+#         def __init__(self, num_classes=17):
+#             super(AlexNet, self).__init__()
+#             self.layer1 = nn.Sequential(
+#                 nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
+#                 nn.BatchNorm2d(96),
+#                 nn.ReLU(),
+#                 nn.MaxPool2d(kernel_size = 3, stride = 2))
+#             self.layer2 = nn.Sequential(
+#                 nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+#                 nn.BatchNorm2d(256),
+#                 nn.ReLU(),
+#                 nn.MaxPool2d(kernel_size = 3, stride = 2))
+#             # self.layer3 = nn.Sequential(
+#             #     nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+#             #     nn.BatchNorm2d(384),
+#             #     nn.ReLU())
+#             # self.layer4 = nn.Sequential(
+#             #     nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+#             #     nn.BatchNorm2d(384),
+#             #     nn.ReLU())
+#             # self.layer5 = nn.Sequential(
+#             #     nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+#             #     nn.BatchNorm2d(256),
+#             #     nn.ReLU(),
+#             #     nn.MaxPool2d(kernel_size = 3, stride = 2))
+#             self.fc = nn.Sequential(
+#                 #nn.Dropout(0.5),
+#                 #nn.Linear(9216, 4096),
+#                 nn.Linear(6400, 4096),
+#                 nn.ReLU())
+#             self.fc1 = nn.Sequential(
+#                 nn.Dropout(0.5),
+#                 nn.Linear(4096, num_classes),
+#                 nn.Softmax())
+#             # self.fc2= nn.Sequential(
+#             #     nn.Linear(4096, num_classes))
+
+#         def forward(self, x):
+#             out = self.layer1(x)
+#             out = self.layer2(out)
+#             #out = self.layer3(out)
+#            #out = self.layer4(out)
+#             #out = self.layer5(out)
+#             #print("After Conv shape:", out.shape)
+#             out = out.reshape(out.size(0), -1)
+#             #print("After layer2 shape:", out.shape)
+#             out = self.fc(out)
+#             out = self.fc1(out)
+#             #out = self.fc2(out)
+#             return out
         
+
+
 num_classes = 17
 num_epochs = 20
 batch_size = 64
@@ -218,7 +273,7 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=0
 total_step = len(train_loader)
 
 
-total_step = len(train_loader)
+
 
 if __name__ == '__main__':
     for epoch in range(num_epochs):
